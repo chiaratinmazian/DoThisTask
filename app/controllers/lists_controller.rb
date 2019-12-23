9d35a43103948f4b48171a7e1231c96c54ef2c49
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-12-23 19:18:03 +0100

    list and create lists of tasks

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,27 @@
+class ListsController < ApplicationController
+  def index
+    @lists = current_user.lists
+  end
+
+  def new
+    @list = List.new
+    @list.user = current_user
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :user_id)
+  end
+
+end
