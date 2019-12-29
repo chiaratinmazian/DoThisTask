e21324a67d8f4a98677ed9362c44f6607eaab439
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-12-29 21:49:36 +0100

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+User.destroy_all
+List.destroy_all
+Task.destroy_all
+
+puts 'Creating data...'
+
+chiara = User.new(
+  username: "Chiara",
+  email: "chiara.tinmazian@gmail.com",
+  password: "password",
+  )
+chiara.save!
+
+remi = User.new(
+  username: "Remi",
+  email: "remi.arnaud@mellonmellon.com",
+  password: "password",
+  )
+remi.save!
+
+thomas = User.new(
+  username: "Thomas",
+  email: "tf@wearebold.co",
+  password: "password",
+  )
+thomas.save!
+
+florence = User.new(
+  username: "Florence",
+  email: "fs@wearebold.co",
+  password: "password",
+  )
+florence.save!
+
+puts 'Database created !!'
